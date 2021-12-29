a58b47b452a531aa470c64cca96067e3cf53adaf
yassine-mabrouk <mabroukyassine70@gmail.com>
2021-12-29 07:07:32 +0100

    first commit

---
+++ src/main/java/com/example/compte/CompteCqrsEventSourcingAxonApplication.java
@@ -0,0 +1,13 @@
+package com.example.compte;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompteCqrsEventSourcingAxonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CompteCqrsEventSourcingAxonApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/compte/CompteCqrsEventSourcingAxonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.compte;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompteCqrsEventSourcingAxonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
